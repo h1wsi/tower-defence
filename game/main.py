0aab9e6745b8457ebdf2b78f7d5f3d263e5f8fcc
exception <iup5drep8.ne@gmail.com>
2023-12-07 02:15:52 +0300

    update the game and added the screen of new game

--- game/main.py
+++ game/main.py
@@ -1,47 +1,20 @@
-import pygame
-from enemy import Enemy
 
-pygame.init()
+import turtle
 
-# настройки игровой панели
-HIGHT, WEIGHT = 600, 700
-FPS = 60
-walking_points = [(100, 100), (500, 300), (500, 200), (300, 400)]
+turtle.speed(0)
+turtle.bgcolor("white")
+turtle.bgpic("src/space.png")
+turtle.setundobuffer(1)
+turtle.ht()
 
-window = pygame.display.set_mode((WEIGHT, HIGHT))
-clock = pygame.time.Clock()
+class Screen(turtle.Turtle):
+	def __init__(self, object, color, x0, y0):
+		turtle.Turtle.__init__(self, shape = object)
+		self.speed(0)
+		self.penup()
+		self.color(color)
+		self.fd(0)
+		self.goto(x0, y0)
+		self.speed = 0
 
-# изображения
-enemy_img = pygame.image.load("src/tank.png").convert_alpha()
-
-# группы
-enemy_grp = pygame.sprite.Group()
-
-# персонажи
-enemy = Enemy(walking_points, enemy_img)
-enemy_grp.add(enemy)
-
-
-play = True
-while play:
-
-    window.fill("white")
-    
-    # движение вражеского персонажа
-    pygame.draw.lines(window, "black", False, walking_points)
-
-    enemy_grp.update()
-    enemy_grp.draw(window)
-
-    for enemy in enemy_grp:
-        enemy.move()
-
-    for event in pygame.event.get():
-
-        if event.type == pygame.QUIT:
-            play = False
-
-    clock.tick(FPS)
-    pygame.display.flip()
-
-pygame.quit()
+prn = input("")
